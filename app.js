7a7ecd3602955487d8d36d27ba5e5de51ce6a358
MahmoudBL83 <95527734+MahmoudBL83@users.noreply.github.com>
2022-03-13 16:29:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ signin.addEventListener('click',async()=>{
         let acctoken= y.access_token;
         localStorage.setItem('storedItem',acctoken);
         signin.style='display:none;';
-        dashboard.style='display:block;position: absolute;right:10%;cursor:pointer;background-color: green;border:3px solid 777;border-radius: 40px;padding:20px;color:white;cursor: pointer;';
+        dashboard.style='display:block;position: absolute;right:10%;cursor:pointer;background-color: #f57f15;border:3px solid 777;border-radius: 40px;padding:20px;color:white;cursor: pointer;';
     } catch (error) {
         console.log(error);
     }
