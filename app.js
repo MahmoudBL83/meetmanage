27e76c4015e6b0e31e3f13faee0944fe54aead84
MahmoudBL83 <95527734+MahmoudBL83@users.noreply.github.com>
2022-04-18 21:29:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -356,16 +356,11 @@ document.querySelector('#memberships').addEventListener('click',async()=>{
 				'Authorization': `Bearer ${sessionStorage.getItem('storedItem')}`
 			},
             body:JSON.stringify({
-                'isModerator':!sessionStorage['booleans'+num]
+                'isModerator':!!sessionStorage['booleans'+num]
             })
             })
             try {
-                if(sessionStorage['booleans'+num]=false){
-                    sessionStorage['booleans'+num]=true;
-                }
-                if(sessionStorage['booleans'+num]=true){
-                    sessionStorage['booleans'+num]=true;
-                }
+                sessionStorage['booleans'+num]=!!sessionStorage['booleans'+num];
             } catch (error) {
                 console.log(error);
             }
